c93ca28b93d8ef736ab8ebc43b948c4ba6fd31a3
ravinder <kabirsood19@gmail.com>
2022-02-27 12:17:56 +0545

    minor changes

--- routes/DownloadFile.js
+++ routes/DownloadFile.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const router = require("express").Router();
 const Files = require("../databse/mydb");
 const path = require("path");
--- routes/SaveFile.js
+++ routes/SaveFile.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const router = require("express").Router();
 const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
